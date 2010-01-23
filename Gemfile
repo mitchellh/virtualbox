acac13b4272509c64dbf2d4807054e6b2ef5c303
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2010-01-22 16:09:10 -0800

    Initial commit. Basic gem layout.

---
+++ Gemfile
@@ -0,0 +1,11 @@
+# Gems required for testing only. To install run
+# gem bundle test
+only :test do
+  gem "contest", ">= 0.1.2"
+  gem "mocha"
+  gem "ruby-debug", ">= 0.10.3" if RUBY_VERSION < '1.9'
+end
+
+# Makes sure that our code doesn't request gems outside
+# of our dependency list.
+disable_system_gems
---
+++ test/test_helper.rb
@@ -0,0 +1,22 @@
+begin
+  require File.join(File.dirname(__FILE__), '..', 'vendor', 'gems', 'environment')
+rescue LoadError
+  puts <<-ENVERR
+==================================================
+ERROR: Gem environment file not found!
+
+This gem uses bundler to handle gem dependencies. To setup the
+test environment, please run `gem bundle test` If you don't
+have bundler, you can install that with `gem install bundler`
+==================================================
+ENVERR
+  exit
+end
+
+# ruby-debug, not necessary, but useful if we have it
+begin
+  require 'ruby-debug'
+rescue LoadError; end
+
+require 'contest'
+require 'mocha'
