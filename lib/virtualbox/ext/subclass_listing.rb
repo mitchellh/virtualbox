720ba2b03c03cbc6985e1d0287b18c1f584e2658
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2010-01-24 01:15:23 -0800

    Added VirtualBox::SubclassListing module which can be included to list subclasses of an object.

---
+++ lib/virtualbox/ext/subclass_listing.rb
@@ -0,0 +1,24 @@
+# From: http://snippets.dzone.com/posts/show/2992
+module VirtualBox::SubclassListing
+  def self.included(base)
+    base.extend ClassMethods
+  end
+  
+  module ClassMethods
+    def subclasses(direct = false)
+      classes = []
+      if direct
+        ObjectSpace.each_object(Class) do |c|
+          next unless c.superclass == self
+          classes << c
+        end
+      else
+        ObjectSpace.each_object(Class) do |c|
+          next unless c.ancestors.include?(self) and (c != self)
+          classes << c
+        end
+      end
+      classes
+    end
+  end
+end
---
+++ test/virtualbox/ext/subclass_listing_test.rb
@@ -0,0 +1,25 @@
+require File.join(File.dirname(__FILE__), '..', '..', 'test_helper')
+require 'virtualbox/ext/subclass_listing'
+
+class SubclassListingTest < Test::Unit::TestCase
+  class A
+    include VirtualBox::SubclassListing
+  end
+  class B < A; end
+  class C < B; end
+  class D < A; end
+  class E
+    include VirtualBox::SubclassListing
+  end
+  class F < E; end
+  
+  should "list subclasses, including sub-subclasses, etc" do
+    assert_equal [F], E.subclasses
+    assert_equal [C], B.subclasses
+    assert_equal [B, C, D], A.subclasses.sort_by { |c| c.name }
+  end
+  
+  should "list direct subclasses if flag is set" do
+    assert_equal [B, D], A.subclasses(true).sort_by { |c| c.name }
+  end
+end
