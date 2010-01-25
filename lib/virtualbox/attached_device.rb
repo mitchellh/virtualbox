6ae2846ddee449dbeae5cb23cb19b9e7165036e8
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2010-01-24 18:26:20 -0800

    StorageController and AttachedDevice documentation

--- lib/virtualbox/attached_device.rb
+++ lib/virtualbox/attached_device.rb
@@ -1,4 +1,43 @@
 module VirtualBox
+  # Represents an device which is attached to a storage controller. An example
+  # of such a device would be a CD or hard drive attached to an IDE controller.
+  #
+  # **Currently, attached devices can not be created from scratch. The only way
+  # to access them is through relationships with other models such as
+  # {StorageController}.**
+  #
+  # # Attributes and Relationships
+  #
+  # Properties of the model are exposed using standard ruby instance
+  # methods which are generated on the fly. Because of this, they are not listed
+  # below as available instance methods. 
+  #
+  # These attributes can be accessed and modified via standard ruby-style
+  # `instance.attribute` and `instance.attribute=` methods. The attributes are
+  # listed below.
+  #
+  # Relationships are also accessed like attributes but can't be set. Instead,
+  # they are typically references to other objects such as an {AttachedDevice} which
+  # in turn have their own attributes which can be modified.
+  #
+  # ## Attributes
+  #
+  # This is copied directly from the class header, but lists all available
+  # attributes. If you don't understand what this means, read {Attributable}.
+  #
+  #     attribute :parent, :readonly => true
+  #     attribute :uuid
+  #     attribute :medium
+  #     attribute :port
+  #
+  # ## Relationships
+  #
+  # In addition to the basic attributes, a virtual machine is related
+  # to other things. The relationships are listed below. If you don't
+  # understand this, read {Relatable}.
+  #
+  #     relationship :image, Image
+  #
   class AttachedDevice < AbstractModel
     attribute :parent, :readonly => true
     attribute :uuid
@@ -7,6 +46,11 @@ module VirtualBox
     relationship :image, Image
     
     class <<self
+      # Populate relationship with another model.
+      #
+      # **This method typically won't be used except internally.**
+      #
+      # @return [Array<AttachedDevice>]
       def populate_relationship(caller, data)
         relation = []
         
@@ -21,11 +65,17 @@ module VirtualBox
         relation
       end
       
+      # Destroy attached devices associated with another model.
+      #
+      # **This method typically won't be used except internally.**
       def destroy_relationship(caller, data, *args)
         data.each { |v| v.destroy(*args) }
       end
     end
     
+    # Since attached devices can not be created from scratch yet, this
+    # method should never be called. Instead access attached devices
+    # through relationships from other models such as {StorageController}.
     def initialize(index, caller, data)
       super()
       
@@ -37,6 +87,13 @@ module VirtualBox
       })
     end
     
+    # Destroys the attached device. By default, this only removes any
+    # media inserted within the device, but does not destroy it. This
+    # option can be specified, however, through the `destroy_image`
+    # option.
+    #
+    # @option options [Boolean] :destroy_image (false) If true, will also
+    #   destroy the image associated with device.
     def destroy(options={})
       # parent = storagecontroller
       # parent.parent = vm
--- lib/virtualbox/storage_controller.rb
+++ lib/virtualbox/storage_controller.rb
@@ -1,4 +1,44 @@
 module VirtualBox
+  # Represents a single storage controller which can be attached to a
+  # virtual machine.
+  # 
+  # **Currently, storage controllers can not be created from scratch.
+  # Therefore, the only way to use this model is through a relationship
+  # of a {VM} object.**
+  #
+  # # Attributes and Relationships
+  #
+  # Properties of the storage controller are exposed using standard ruby instance
+  # methods which are generated on the fly. Because of this, they are not listed
+  # below as available instance methods. 
+  #
+  # These attributes can be accessed and modified via standard ruby-style
+  # `instance.attribute` and `instance.attribute=` methods. The attributes are
+  # listed below.
+  #
+  # Relationships are also accessed like attributes but can't be set. Instead,
+  # they are typically references to other objects such as an {AttachedDevice} which
+  # in turn have their own attributes which can be modified.
+  #
+  # ## Attributes
+  #
+  # This is copied directly from the class header, but lists all available
+  # attributes. If you don't understand what this means, read {Attributable}.
+  #
+  #     attribute :parent, :readonly => true
+  #     attribute :name
+  #     attribute :type
+  #     attribute :max_ports, :populate_key => :maxportcount
+  #     attribute :ports, :populate_key => :portcount
+  #
+  # ## Relationships
+  #
+  # In addition to the basic attributes, a virtual machine is related
+  # to other things. The relationships are listed below. If you don't
+  # understand this, read {Relatable}.
+  #
+  #     relationship :devices, AttachedDevice, :dependent => :destroy
+  #
   class StorageController < AbstractModel
     attribute :parent, :readonly => true
     attribute :name
@@ -8,6 +48,11 @@ module VirtualBox
     relationship :devices, AttachedDevice, :dependent => :destroy
     
     class <<self
+      # Populates a relationship with another model.
+      #
+      # **This method typically won't be used except internally.**
+      #
+      # @return [Array<StorageController>]
       def populate_relationship(caller, data)
         relation = []
         
@@ -22,11 +67,18 @@ module VirtualBox
         relation
       end
       
+      # Destroys a relationship with another model.
+      #
+      # **This method typically won't be used except internally.**
       def destroy_relationship(caller, data, *args)
         data.each { |v| v.destroy(*args) }
       end
     end
     
+    # Since storage controllers still can't be created from scratch,
+    # this method shouldn't be called. Instead, storage controllers
+    # can be retrieved through relationships of other models such
+    # as {VM}.
     def initialize(index, caller, data)
       super()
       
@@ -49,6 +101,11 @@ module VirtualBox
       }))
     end
     
+    # Extracts related devices for a storage controller.
+    #
+    # **This method typically won't be used except internally.**
+    #
+    # @return [Hash]
     def extract_devices(index, data)
       name = data["storagecontrollername#{index}".downcase.to_sym].downcase
       
