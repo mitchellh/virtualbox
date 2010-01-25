66e0a49ba7e59d10d2342ec8274f2bb615846650
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2010-01-24 17:41:00 -0800

    Image documentation

--- lib/virtualbox/image.rb
+++ lib/virtualbox/image.rb
@@ -1,6 +1,19 @@
 require 'virtualbox/ext/subclass_listing'
 
 module VirtualBox
+  # An abstract class which encapsulates the shared behaviour of
+  # images such as {HardDrive} and {DVD}.
+  #
+  # ## Attributes
+  #
+  # All images expose the following attributes. If you don't know how to read
+  # this than read {Attributable}.
+  #
+  #     attribute :uuid, :readonly => true
+  #     attribute :location
+  #     attribute :accessible, :readonly => true
+  #
+  # @abstract
   class Image < AbstractModel
     include SubclassListing
     
@@ -9,14 +22,31 @@ module VirtualBox
     attribute :accessible, :readonly => true
     
     class <<self
+      # Parses the raw output of virtualbox into image objects. Used by
+      # subclasses to parse the output of their respective listing functions.
+      #
+      # **This method typically won't be used except internally.**
+      #
+      # @return [Array<Image>]
       def parse_raw(raw)
         parse_blocks(raw).collect { |v| new(v) }
       end
       
+      # Parses the blocks of the output from virtualbox. VirtualBox outputs
+      # image listing in "blocks" which are then parsed down to their attributes.
+      #
+      # **This method typically won't be used except internally.**
+      #
+      # @return [Array<Hash>]
       def parse_blocks(raw)
         raw.split(/\n\n/).collect { |v| parse_block(v.chomp) }.compact
       end
       
+      # Parses a single block from VirtualBox output. 
+      #
+      # **This method typically won't be used except internally.**
+      #
+      # @return [Hash]
       def parse_block(block)
         return nil unless block =~ /^UUID:/i
         hd = {}
@@ -37,6 +67,10 @@ module VirtualBox
       
       # Searches the subclasses which implement all method, searching for
       # a matching UUID and returning that as the relationship.
+      #
+      # **This method typically won't be used except internally.**
+      #
+      # @return [Array<Image>]
       def populate_relationship(caller, data)
         return nil if data[:uuid].nil?
         
@@ -49,6 +83,8 @@ module VirtualBox
       end
     end
     
+    # **This should never be called directly on {Image}.** Instead, initialize
+    # one of the subclasses.
     def initialize(info=nil)
       super()
       
