db5aa3694537f906f9fc4237790eff8c0715328f
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2010-03-15 22:44:00 -0700

    Added Platform class to determine platform to ease testability

---
+++ lib/virtualbox/ext/platform.rb
@@ -0,0 +1,19 @@
+class VirtualBox::Platform
+  class <<self
+    def mac?
+      platform.include?("darwin")
+    end
+
+    def windows?
+      platform.include?("mswin") || platform.include?("mingw")
+    end
+
+    def linux?
+      platform.include?("linux")
+    end
+
+    def platform
+      RUBY_PLATFORM.downcase
+    end
+  end
+end
---
+++ test/virtualbox/ext/platform_test.rb
@@ -0,0 +1,42 @@
+require File.join(File.dirname(__FILE__), '..', '..', 'test_helper')
+require 'virtualbox/ext/platform'
+
+class PlatformTest < Test::Unit::TestCase
+  context "mac" do
+    should "return true if it contains darwin" do
+      success = ["i386-darwin", "i686-darwin10", "imadarwin"]
+      success.each do |item|
+        VirtualBox::Platform.stubs(:platform).returns(item)
+        assert VirtualBox::Platform.mac?
+      end
+    end
+  end
+
+  context "windows" do
+    should "return true if it contains mswin" do
+      success = ["i386-mswin32", "i686-mswin64", "imswin"]
+      success.each do |item|
+        VirtualBox::Platform.stubs(:platform).returns(item)
+        assert VirtualBox::Platform.windows?
+      end
+    end
+
+    should "return true if it contains mingw" do
+      success = ["i386-mingw32", "i686-mingw64", "mingw"]
+      success.each do |item|
+        VirtualBox::Platform.stubs(:platform).returns(item)
+        assert VirtualBox::Platform.windows?
+      end
+    end
+  end
+
+  context "linux" do
+    should "return true if it contains linux" do
+      success = ["i386-linux", "i686-linux241", "linux"]
+      success.each do |item|
+        VirtualBox::Platform.stubs(:platform).returns(item)
+        assert VirtualBox::Platform.linux?
+      end
+    end
+  end
+end
