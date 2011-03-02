06313812d2138a61944e7e96c81ea103ded4e6ed
geemus <geemus@gmail.com>
2011-03-01 18:25:51 -0800

    [4.0.x] add DataType

---
+++ lib/virtualbox/com/interface/4.0.x/data_type.rb
@@ -0,0 +1,11 @@
+module VirtualBox
+  module COM
+    module Interface
+      module Version_4_0_X
+        class DataType < AbstractEnum
+          map [:int32, :int8, :string]
+        end
+      end
+    end
+  end
+end
