dcca94e347b7daf8e61f45e3b7b04da46a0d4216
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2011-07-09 10:34:06 -0700

    4.1.x: NetworkAdapter

--- TODO.md
+++ TODO.md
@@ -2,3 +2,5 @@
 
 * NetworkAdapter uses `network_adapter_counter` which is no longer supported.
   It needs to use `get_max_network_adapters` instead.
+* NetworkAdapter `attach_to_*` methods are gone. Use `attachment_type` property
+  instead.
--- lib/virtualbox/com/interface/4.1.x/NetworkAdapter.rb
+++ lib/virtualbox/com/interface/4.1.x/NetworkAdapter.rb
@@ -9,25 +9,24 @@ module VirtualBox
           property :slot, T_UINT32, :readonly => true
           property :enabled, T_BOOL
           property :mac_address, WSTRING
-          property :attachment_type, :NetworkAttachmentType, :readonly => true
-          property :host_interface, WSTRING
+          property :attachment_type, :NetworkAttachmentType
+          property :bridged_interface, WSTRING
+          property :host_only_interface, WSTRING
           property :internal_network, WSTRING
           property :nat_network, WSTRING
-          property :vde_network, WSTRING
+          property :generic_driver, WSTRING
           property :cable_connected, T_BOOL
           property :line_speed, T_UINT32
+          property :promisc_mode_policy, :NetworkAdapterPromiscModePolicy
           property :trace_enabled, T_BOOL
           property :trace_file, WSTRING
           property :nat_driver, :NATEngine, :readonly => true
           property :boot_priority, T_UINT32
-          property :bandwidth_limit, T_UINT32
+          property :bandwidth_group, :BandwidthGroup
 
-          function :attach_to_nat, nil, []
-          function :attach_to_bridged_interface, nil, []
-          function :attach_to_internal_network, nil, []
-          function :attach_to_host_only_interface, nil, []
-          function :attach_to_vde, nil, []
-          function :detach, nil, []
+          function :get_property, WSTRING, [WSTRING]
+          function :set_property, nil, [WSTRING, WSTRING]
+          function :get_properties, [WSTRING], [WSTRING, [:out, WSTRING]]
         end
       end
     end
