82ad2ed096618823fea280a524e637a49327a119
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2011-07-22 23:13:46 -0700

    v0.9.1

--- lib/virtualbox/version.rb
+++ lib/virtualbox/version.rb
@@ -1,5 +1,5 @@
 module VirtualBox
-  VERSION = "0.9.0"
+  VERSION = "0.9.1"
 
   # Returns a boolean denoting whether the current VirtualBox
   # version is supported or not. This will return `false` if the
